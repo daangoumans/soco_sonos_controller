e6c105123d4c27f52627497c0046aaac5e7457ef
Daan Goumans <daan.goumans@hotmail.com>
2017-11-03 13:52:07 +0100

    Update play.py

--- play.py
+++ play.py
@@ -1,22 +1,35 @@
 #!/usr/bin/env python
 import soco
+from soco.music_services import MusicService
+import sys
 
-# Get device and services
-device = soco.discovery.any_soco()
-print(device)
-services = device.music_services.get_all_music_services_names()
-print(services)
+speaker_name = ''
+song_url=''
+
+def find_soc_speaker():
+    # Get device and services
+    speakers = soco.discover()
+    for speaker in speakers:
+        if speaker.player_name == speaker_name:
+            return speaker
+    if not soc_speaker:
+        sys.exit()
 
 
-# Use service
-spotify = MusicService('Spotify')
 
-if 'chill' in input:
-    response =  spotify.get_metadata(item_id='spotify:user:spotify:playlist:0FQk6BADgIIYd3yTLCThjg')
-    print(response)
+speaker = find_soc_speaker()
 
-request_uri = spotify.sonos_uri_from_id(response)
-device.clear_queue()
-device.add_to_queue(request_uri)
-device.play_mode("SHUFFLE_NOREPEAT")
-device.play()
+if len(sys.argv) <= 1:
+    print('No Playlist Selected')
+    sys.exit()
+else:
+    style = sys.argv[1]
+    stations = speaker.get_favorite_radio_stations(0, 2)
+    if (str(style) == 'chill'):
+        song_url = stations['favorites'][0]['uri']
+    if (str(style) == 'other'):
+        song_url = stations['favorites'][1]['uri']
+
+print(song_url)
+speaker.volume = 15
+speaker.play_uri(uri=song_url,force_radio=True)
